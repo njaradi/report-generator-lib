8b08d6549a6f4e9d86016bb32ca8f16af99cb14c
duuunjaaa <62660866+duuunjaaa@users.noreply.github.com>
2024-11-01 15:31:37 +0100

    nista haha

--- report_spec/src/main/kotlin/spec/ReportGeneratorInterface.kt
+++ report_spec/src/main/kotlin/spec/ReportGeneratorInterface.kt
@@ -1,5 +1,38 @@
 package org.example.spec
 
+import java.sql.ResultSet
+import java.sql.ResultSetMetaData
+
 interface ReportGeneratorInterface {
     //TODO: the project
+    fun generateReport(data: Map<String, List<String>>, destination: String, header: Boolean, title: String? = null, summary: String? = null)
+
+
+    fun generateReport(data: ResultSet, destination: String, header: Boolean, title: String? = null, summary: String? = null){
+        val preparedData = prepareData(data)
+        generateReport(preparedData, destination, header, title, summary)
+    }
+
+
+    /** */
+    private fun prepareData(resultSet: ResultSet): Map<String, List<String>> {
+        val reportData = mutableMapOf<String, MutableList<String>>()
+
+        val metaData: ResultSetMetaData = resultSet.metaData
+        val columnCount = metaData.columnCount
+
+        for (i in 1..columnCount) {
+            val columnName = metaData.getColumnName(i)
+            reportData[columnName] = mutableListOf()
+        }
+
+        while (resultSet.next()) {
+            for (i in 1..columnCount) {
+                val columnName = metaData.getColumnName(i)
+                reportData[columnName]!!.add(resultSet.getString(i))
+            }
+        }
+
+        return reportData
+    }
 }
